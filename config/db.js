be094c2815056aa7f6af44d59a68dc54838fb96f
AxelSalim <adjakidjememiaghe@gmail.com>
2025-09-01 22:49:12 +0100

    Refactoriser la configuration de la base de données pour prendre en charge plusieurs environnements (développement, test, production) à l'aide de variables d'environnement pour les paramètres de production.

--- config/db.js
+++ config/db.js
@@ -1,13 +1,27 @@
-const mongoose = require("mongoose");
+require("dotenv").config();
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connected");
-  } catch (err) {
-    console.error("❌ Error connecting DB:", err.message);
-    process.exit(1);
-  }
+module.exports = {
+  development: {
+    username: "root",
+    password: null,
+    database: "task_manager",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    logging: false,
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "task_manager_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+  },
 };
-
-module.exports = connectDB;
