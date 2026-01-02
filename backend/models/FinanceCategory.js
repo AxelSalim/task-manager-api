775e09390973913e9a83a9f89103b48e4b03858a
AxelSalim <adjakidjememiaghe@gmail.com>
2026-01-02 18:15:13 +0100

    feat(models): add FinanceCategory model with associations to User and FinanceTransaction

---
+++ backend/models/FinanceCategory.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+const TYPES = ['revenus', 'factures', 'depenses', 'epargnes', 'credits'];
+
+module.exports = (sequelize, DataTypes) => {
+  class FinanceCategory extends Model {
+    static associate(models) {
+      FinanceCategory.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
+      FinanceCategory.hasMany(models.FinanceTransaction, {
+        foreignKey: 'categoryId',
+        as: 'transactions',
+      });
+    }
+  }
+  FinanceCategory.init(
+    {
+      userId: { type: DataTypes.INTEGER, allowNull: false },
+      name: { type: DataTypes.STRING, allowNull: false },
+      type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: { isIn: [TYPES] },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'FinanceCategory',
+      tableName: 'FinanceCategories',
+      timestamps: true,
+    }
+  );
+  return FinanceCategory;
+};
