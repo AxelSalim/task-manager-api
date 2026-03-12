0b2737ff317c40ed4a58430b7b3207e75db6856a
AxelSalim <adjakidjememiaghe@gmail.com>
2026-03-12 12:13:42 +0100

    chore: Remove test-reminder.bat script as it is no longer needed

--- electron/scripts/test-reminder.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Double-cliquer pour tester les rappels SPARK (notification + planification 1 min)
-cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0test-reminder.ps1" -All
-pause
